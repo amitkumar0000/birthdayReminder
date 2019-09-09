93971c44e82d528562b86e7f5cc230014d255bd2
amit <amit.ap1924@gmail.com>
2019-09-09 22:55:47 +0530

    Resolved remaining days issue

--- app/src/main/java/com/birthday/common/DateUtils.kt
+++ app/src/main/java/com/birthday/common/DateUtils.kt
@@ -4,7 +4,20 @@ import java.util.Date
 
 object DateUtils {
   fun getRemainingDays(dob: Date, cDate: Date):Int{
-    return Math.abs(absoluteDay(dob.month,dob.date) - absoluteDay(cDate.month,cDate.date))
+
+    var dAbsDays=absoluteDay(dob.month, dob.date)
+
+    if(cDate.month > dob.month || (dob.month == cDate.month && cDate.date>dob.date)){
+       dAbsDays += 365
+    }
+    
+    var cDays = absoluteDay(cDate.month,cDate.date)
+
+    return Math.abs(dAbsDays - cDays)
+  }
+
+  private fun isLeapYear(year: Int): Boolean {
+    return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))
   }
 
   fun absoluteDay(month: Int, day: Int): Int {
@@ -13,25 +26,25 @@ object DateUtils {
     if (month == 2)
       return day + 31
     if (month == 3)
-      return day + 60
+      return day + 60 
     if (month == 4)
-      return day + 91
+      return day + 91 
     if (month == 5)
-      return day + 121
+      return day + 121 
     if (month == 6)
-      return day + 152
+      return day + 152 
     if (month == 7)
-      return day + 182
+      return day + 182 
     if (month == 8)
-      return day + 213
+      return day + 213 
     if (month == 9)
-      return day + 244
+      return day + 244 
     if (month == 10)
-      return day + 274
+      return day + 274 
     if (month == 11)
-      return day + 305
+      return day + 305 
     return if (month == 12)
-      day + 335
+      day + 335 
     else
       0
   }
