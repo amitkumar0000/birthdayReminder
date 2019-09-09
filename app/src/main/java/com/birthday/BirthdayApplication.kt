f4bc4cde89d411e0e0e4f9c52b7f6b693b750452
amit <amit.ap1924@gmail.com>
2019-09-10 00:31:13 +0530

    setup crashlytics

--- app/src/main/java/com/birthday/BirthdayApplication.kt
+++ app/src/main/java/com/birthday/BirthdayApplication.kt
@@ -4,8 +4,15 @@ import android.app.Application
 import com.birthday.ui.birthdayFeed.di.BirthdayStorageComponent
 import com.birthday.ui.birthdayFeed.di.BirthdayStorageModule
 import com.birthday.di.modules.ContextModule
+import com.birthday.ui.BuildConfig
 import com.birthday.ui.birthdayFeed.di.DaggerBirthdayStorageComponent
+import com.google.firebase.FirebaseApp
 import timber.log.Timber
+import io.fabric.sdk.android.Fabric
+import com.crashlytics.android.core.CrashlyticsCore
+import com.crashlytics.android.Crashlytics
+
+
 
 class BirthdayApplication : Application() {
 
@@ -13,15 +20,29 @@ class BirthdayApplication : Application() {
     lateinit var component: BirthdayStorageComponent
   }
 
-
-
   override fun onCreate() {
     super.onCreate()
+    FirebaseApp.initializeApp(this);
 
-    Timber.plant(Timber.DebugTree())
+    setupCrashlytics()
+
+    if (BuildConfig.DEBUG)
+      Timber.plant(Timber.DebugTree())
 
     component = DaggerBirthdayStorageComponent.builder().contextModule(ContextModule(this))
       .birthdayStorageModule(BirthdayStorageModule()).build()
   }
 
+  fun setupCrashlytics(){
+    // Set up Crashlytics, disabled for debug builds
+    val crashlyticsKit = Crashlytics.Builder()
+      .core(
+        CrashlyticsCore.Builder()
+          .build()
+      )
+      .build()
+
+// Initialize Fabric with the debug-disabled Crashlytics
+    Fabric.with(this, crashlyticsKit) // Correct initializer!
+  }
 }
